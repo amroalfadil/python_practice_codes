42b5fca816798ab2f92febef966140314f17f71e
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-14 09:09:10 +0000

    save changes

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 x = input("what's x? ")
 y = input("what's y? ")
-
-z = x + y
+#not because we ask for user input it is str not in or a number 
+# we need to add a function to make them a number by using int()
+z = int(x) + int(y)
 
 print(z)
