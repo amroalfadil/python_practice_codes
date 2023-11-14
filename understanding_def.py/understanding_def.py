ab16c017025cf7e0a275e28a4190f6fe5fa78269
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-14 10:38:59 +0000

    fixed an error

--- understanding_def.py/understanding_def.py
+++ understanding_def.py/understanding_def.py
@@ -1,7 +1,7 @@
 # create a function called hello
 #you can add parameter too
 def hello(to):
-    print("Hello,", to")
+    print("Hello,", to)
 name = input("What's your name? ")
 hello()
 print(name)
