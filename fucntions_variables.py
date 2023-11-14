d302d4e370c4ec885157fde00624eec44ddb0fbe
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-14 08:51:34 +0000

    save calcualtor.py

---
+++ fucntions_variables.py
@@ -0,0 +1,32 @@
+#to create python folder qrite in terminal of the VScode code file_name.py
+#print("hello, world")
+#to run we need to use this command python file_name.py
+#Functions : a verb let you do something in the programe, the function here is print()
+#arguments :  input to the function influnace its behavior 
+#bug: is a mistake in a programe
+#print()
+input("What's your name?")
+print("Hello, world")
+#but this dont print your name  when you run hello world
+#we need to leverage "return values"
+#variables can do this, to save value in  memory or a continer of a value
+#Ask user for thier name
+#say hello to user
+"""
+name = input("What is your name?")
+print("hello,")
+print(name)
+"""
+
+#comments : notes to your self on the code go on top and add a comment, can be also a todo list (pseudocode =to outline your programe in advance)
+"""
+this is a comment too
+"""
+
+#we can solve above programe in many ways
+name = input("What is your name? ") # note we have space here after the question mark
+#print("hello," + name) # but there is missing space you can "Hello, "
+#print("hello, ", name) #when passing multiple arguments if apply space automatically
+print("hello, ")
+print(name)
+
