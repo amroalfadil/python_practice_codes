87c22e43a37d1fddcc8f5e28ea706894c405b0e4
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-16 17:37:02 +0000

    fixed

--- exs_1.py
+++ exs_1.py
@@ -1,8 +1,6 @@
 def printer(counter):
     for count in range(counter):
-        #trying to fix the top 4 lines 
-        count = counter - 1
         print("*"*count)
-#    for count in reversed(range(counter)):
-#        print("*"*count)
+    for count in reversed(range(counter)):
+        print("*"*count)
 printer(5)
