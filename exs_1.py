2efe8fda09f0292014692358230cf7bc5e5972e9
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-17 10:35:33 +0000

    completed

--- exs_1.py
+++ exs_1.py
@@ -1,6 +1,7 @@
+x = int(input("What is the maximum number you would like?")).strip()
 def printer(counter):
     for count in range(counter):
         print("*"*count)
     for count in reversed(range(counter)):
         print("*"*count)
-printer(5)
+printer(x)
