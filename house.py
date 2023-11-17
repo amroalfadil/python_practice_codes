88592611f37080a6ba31da5491295677ae4011c1
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-17 12:03:00 +0000

    completed

---
+++ house.py
@@ -0,0 +1,18 @@
+'''
+using match 
+task: we will create a programe asking the user for thier name
+and output what house to be in in the world of harry potter
+'''
+
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Griffindor")
+elif name == "Hermione":
+    print("Griffindor")
+elif name == "Ron":
+    print("Griffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
