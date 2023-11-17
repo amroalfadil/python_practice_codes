fbf465c26b463dff256b6972a4e0bd12934b008b
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-17 11:48:51 +0000

    created the function

--- parity.py
+++ parity.py
@@ -1,6 +1,18 @@
-x = int(input("What is x? "))
+def main():
+    x = int(input("What is x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
 
-if x % 2 ==0:
-    print("even")
-else:
-    print("Odd")
+def is_even(n):
+    if n % 2 ==0:
+        return True
+    else:
+        return False
+
+
+
+
+
+main()
