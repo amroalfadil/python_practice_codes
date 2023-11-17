a0d8051680f855a4c68b92995b0bad5510aa6319
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-17 11:06:22 +0000

    compare app

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+#boolean expression a question has yes and no answer or true or false action
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
