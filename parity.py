ee5c9992af564919096ead14ef093dfbe3fca7c6
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-17 11:49:38 +0000

    commented

--- parity.py
+++ parity.py
@@ -1,10 +1,11 @@
 def main():
     x = int(input("What is x? "))
+    #you invent a function to keep the logic is_even(x)
     if is_even(x):
         print("Even")
     else:
         print("Odd")
-
+#build the invented function earlier
 def is_even(n):
     if n % 2 ==0:
         return True
