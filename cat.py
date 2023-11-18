87c8baa586c4d440baff006eeb5e62f06edda4ee
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-18 09:54:56 +0000

    programs started

---
+++ cat.py
@@ -0,0 +1,16 @@
+'''
+Task: a program that mewos like a cat
+'''
+'''
+print("meow")
+print("mewo")
+print("meow")
+'''
+# using while 
+
+i = 3
+while i !=0:
+    print("meow")
+    # if you stoped here will go "meow" for ever, 
+    #thats why whe  have decrease iteratively the i as follows
+    i = i - 1
