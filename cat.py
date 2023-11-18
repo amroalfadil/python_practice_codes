1d01781a6c6f386829700bd5ac1506c6df3bb630
Amro <58052565+amroalfadil@users.noreply.github.com>
2023-11-18 10:52:52 +0000

    counting from 3 to 1

--- cat.py
+++ cat.py
@@ -7,10 +7,19 @@ print("mewo")
 print("meow")
 '''
 # using while 
-
+'''
 i = 3
 while i !=0:
     print("meow")
     # if you stoped here will go "meow" for ever, 
     #thats why whe  have decrease iteratively the i as follows
     i = i - 1
+'''
+
+#we can make it to count up instead of down
+i = 0
+while i < 3: # starting from 0 up to 3 not including 3
+    print("meow")
+    # if you stoped here will go "meow" for ever, 
+    #thats why whe  have decrease iteratively the i as follows
+    i = i + 1
